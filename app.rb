0ccce3f7d73d6505664689a77e3d5fce48e1322c
f440 <freq440@gmail.com>
2020-05-23 18:45:19 +0900

    Imprement error pattern test with ar-multitennat

--- app.rb
+++ app.rb
@@ -8,13 +8,15 @@ gemfile(true) do
   git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
   # Activate the gem you are reporting the issue against.
-  gem "rails", "6.0.3"
+  gem "rails", ENV["RAILS_VERSION"] || "6.0.3"
   gem "sqlite3"
+  gem "activerecord-multi-tenant", require: false
 end
 
 require "active_record/railtie"
 require "active_storage/engine"
 require "tmpdir"
+require "activerecord-multi-tenant"
 
 class TestApp < Rails::Application
   config.root = __dir__
@@ -44,18 +46,41 @@ require ActiveStorage::Engine.root.join("db/migrate/20170806125915_create_active
 ActiveRecord::Schema.define do
   CreateActiveStorageTables.new.change
 
-  create_table :users, force: true
+  create_table :tenants
+  create_table :groups, force: true do |t|
+    t.integer :tenant_id, null: false
+  end
+  create_table :users, force: true do |t|
+    t.integer :tenant_id, null: false
+    t.integer :group_id, null: false
+  end
+end
+
+class Tenant < ActiveRecord::Base
+end
+
+class Group < ActiveRecord::Base
+  multi_tenant :tenant
+  has_many :users
 end
 
 class User < ActiveRecord::Base
+  multi_tenant :tenant
+  belongs_to :group
+
   has_one_attached :profile
 end
 
 require "minitest/autorun"
 
 class BugTest < Minitest::Test
-  def test_upload_and_download
-    user = User.create!(
+  def test_eager_load
+    tenant = Tenant.create!
+    group = Group.create!(tenant: tenant)
+
+    User.create!(
+      tenant: tenant,
+      group: group,
       profile: {
         content_type: "text/plain",
         filename: "dummy.txt",
@@ -63,6 +88,6 @@ class BugTest < Minitest::Test
       }
     )
 
-    assert_equal "dummy", user.profile.download
+    assert_equal group.id, Group.eager_load(users: { profile_attachment: :blob }).find(group.id).id
   end
 end
